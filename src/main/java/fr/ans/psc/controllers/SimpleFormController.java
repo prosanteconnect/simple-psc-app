3710f55c7028dcdbf05771bcdf2d6f062c34e906
bbo <bbonche@henix.fr>
2022-11-22 09:59:41 +0100

    basic html impl : index page with PSC connection & basic form page

--- src/main/java/fr/ans/psc/controllers/SimpleFormController.java
+++ src/main/java/fr/ans/psc/controllers/SimpleFormController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SimpleFormController {
 
-    @GetMapping("/form")
+    @GetMapping("/secure/form")
     public String getSimpleFormPage() {
         return "form-page";
     }
